29b690bea822ac1b237230a025d88f0327865fee
Elias Tähtö <elias.tahto@solita.fi>
2024-11-13 15:30:28 +0200

    Add favicon and app icon

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,11 +1,23 @@
+<script>
+  import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+  import { faBowlRice } from '@fortawesome/free-solid-svg-icons';
+
+  const logoIcon = faBowlRice;
+</script>
+
 <!-- Header.svelte -->
 <header class="bg-primary text-accent3 p-4 shadow-md">
   <div class="container mx-auto flex justify-between items-center">
     <!-- App Title -->
-    <h1 class="text-3xl font-bold text-accent3">Anime Viewer</h1>
-
+    <div class="flex justify-between gap-3">
+      <FontAwesomeIcon icon={logoIcon} class="text-3xl text-accent3" />
+      <!-- Logo icon -->
+      <h1 class="text-3xl font-bold text-accent3">Anime Viewer</h1>
+    </div>
     <!-- App Description -->
-    <p class="text-sm italic text-accent2">Explore top-rated anime by season and year</p>
+    <p class="text-sm italic text-accent2">
+      Explore top-rated anime by season and year
+    </p>
 
     <!-- Navigation Links -->
     <nav class="space-x-4">
