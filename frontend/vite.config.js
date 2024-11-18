078b7d44bdd82604f0eb1811d1f03bf20ff2b8a8
egesd <eliastsky@gmail.com>
2024-11-18 08:42:15 +0200

    Modify vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  root: './', // Set root to current directory if needed
   plugins: [svelte()],
   build: {
-    outDir: 'dist', // Ensure output directory is named 'dist'
-  },
+    outDir: 'dist' // Vite's default output directory
+  }
 });
