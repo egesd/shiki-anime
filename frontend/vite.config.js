143fbfc9056f21da77f110f56f70f7fd9b0827af
egesd <eliastky@gmail.com>
2024-11-18 08:23:19 +0200

    Add vercel config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: './', // Set root to current directory if needed
   plugins: [svelte()],
-})
+  build: {
+    outDir: 'dist', // Ensure output directory is named 'dist'
+  },
+});
